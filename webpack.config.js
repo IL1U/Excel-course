bdc9274d7b66a0dc12f79ea997ffe57d3d31913a
ILIU <iliu@yandex.ru>
2021-05-05 22:26:41 +0300

    config webpack: instal plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require ('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-//const CopyPlugin = require("copy-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
 	context: path.resolve(__dirname, 'src'),
@@ -11,16 +12,25 @@ module.exports = {
 		filename: 'bundle.[hash].js',
 		path: path.resolve(__dirname,'dist')
 	},
+	resolve: {
+		extensions: ['.js'],
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+			'@core': path.resolve(__dirname, 'src/core'),
+		}
+	},
 	plugins: [
+		new MiniCssExtractPlugin({
+			filename: 'bundle.[hash].css',
+		}),
 		new CleanWebpackPlugin(),
 		new HtmlWebpackPlugin({
 			template: 'index.html',
 		}),
-		/*new CopyPlugin({
+		new CopyPlugin({
 	      patterns: [
-	        { from: "source", to: "dest" },
-	        { from: "other", to: "public" },
+	        { from: path.resolve(__dirname, 'src/favicon.ico'), to: path.resolve(__dirname, 'dist' },
 	      ],
-	    }),*/
+	    }),
   	],
 }
